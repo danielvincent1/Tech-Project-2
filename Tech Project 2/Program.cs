c090d7eb21945cc5120bc8c77bcf5cb40168f699
Daniel Villavicencio <daniel124@usf.edu>
2021-09-15 19:20:59 -0400

    Added Comments

--- Tech Project 2/Program.cs
+++ Tech Project 2/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+Daniel Villavicencio
+ISM 4300
+Due Date: 09/16/21
+Description: This app tells the user the letter grade of their expected integer grade
+*/
+
+using System;
 
 namespace Tech_Project_2
 {
@@ -10,18 +13,18 @@ namespace Tech_Project_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your expected grade for this class?");
+            Console.WriteLine("What is your expected grade for this class?"); // The app first asks the user what grade they expect
 
-            try
+            try // a try-catch is used in case the user does not enter an int value
             {
-                string input = Console.ReadLine();
+                string input = Console.ReadLine(); // the app takes an input for the user
 
-                int desiredGrade = int.Parse(input);
+                int desiredGrade = int.Parse(input); // the input is read as an integer
 
 
-                if (desiredGrade < 60)
+                if (desiredGrade < 60) // the app starts to go through the conditional statements
                 {
-                    Console.WriteLine("Your expected grade is a F!");
+                    Console.WriteLine("Your expected grade is a F!"); // the loop stops if the value falls in the parameter and displays the letter grade
                 }
                 else if (desiredGrade <= 62)
                 {
@@ -67,11 +70,11 @@ namespace Tech_Project_2
                 {
                     Console.WriteLine("Your expected grade is a A!");
                 }                
-            }
-            catch (Exception ex)
+            } // end try
+            catch (Exception ex) // if the input does not match any of those values, it goes into the catch
             {
-                Console.WriteLine(ex.Message);
-            }
+                Console.WriteLine(ex.Message); // the system tells the user the input was wrong or what the error is
+            } // end catch
         }
     }
 }
